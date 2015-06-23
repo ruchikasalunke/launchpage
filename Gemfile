fa82bf547454ba8130773c502e3b365d4d296547
Your Name <ruchikasalunke@gmail.com>
2015-06-23 03:59:54 +0000

    Update for Heroku

--- Gemfile
+++ Gemfile
@@ -42,14 +42,14 @@ group :development, :test do
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
- 
+  gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   
   gem 'sqlite3'
   
-   gem 'web-console', '2.0.0.beta3'
+  
 
 end
 
--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module CssTest
   class Application < Rails::Application
     
-     config.web_console.whitelisted_ips = '192.168.0.0/16'
+    config.web_console.whitelisted_ips = '192.168.0.0/16'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
