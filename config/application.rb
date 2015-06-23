35f0033cb2049a71115a18c021927cfed817eba9
Your Name <ruchikasalunke@gmail.com>
2015-06-23 03:52:07 +0000

    Update for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module CssTest
     config.assets.enabled = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.automount = true
   end
 end
